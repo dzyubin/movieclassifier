52eef832570f0e361d7b024f0077e5dd8168c08e
Volodymyr Dziubin <Volodymyr_Dziubin@epam.com>
2022-01-09 15:16:54 +0200

    upd

--- face_tracking.py
+++ face_tracking.py
@@ -11,3 +11,10 @@ from IPython import display
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print('Running on device: {}'.format(device))
+
+# since MTCNN is a collection of neural nets and other code, the device must be passed in the following way to enable copying of objects when needed internally.
+mtcnn = MTCNN(keep_all=True, device='cpu')
+
+#loading a video with some faces in it. The mmcv PyPI package by mmlabs is used to read the video frames (it can be installed with pip install mmcv). Frames are then converted to PIL images
+video = mmcv.VideoReader('../uploads/video.mp4')
+frames = [Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)) for frame in video]
