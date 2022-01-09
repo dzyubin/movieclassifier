a537bb415a6408e84cd431fa2f776d69f6186de3
Volodymyr Dziubin <Volodymyr_Dziubin@epam.com>
2022-01-09 15:10:43 +0200

    upd

--- face_tracking.py
+++ face_tracking.py
@@ -1,7 +1,9 @@
 from facenet_pytorch import MTCNN
 import mmcv, cv2
 import sys
-print("Python version")
-print (sys.version)
-print("Version info.")
-print (sys.version_info)
+from PIL import Image, ImageDraw
+from IPython import display
+# print("Python version")
+# print (sys.version)
+# print("Version info.")
+# print (sys.version_info)
