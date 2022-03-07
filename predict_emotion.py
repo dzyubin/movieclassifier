2246e6ce9b0c2666be3ff25b9cf4082d77d99201
Volodymyr Dziubin <vovadzyubin@gmail.com>
2022-03-07 13:08:49 +0200

    upd

--- predict_emotion.py
+++ predict_emotion.py
@@ -17,7 +17,7 @@ def predict_emotion(file):
   img = image.load_img(file, color_mode="grayscale", target_size=(48, 48))
   x = image.img_to_array(img)
 
-#   x = np.expand_dims(x, axis = 0)
+  x = np.expand_dims(x, axis = 0)
   x /= 255
   print(x)
   print(x.shape)
