b9cee7614729c34b2790bf28ba83bcc44676dd9b
Volodymyr Dziubin <vovadzyubin@gmail.com>
2022-03-07 10:20:43 +0200

    upd

--- face_tracking.py
+++ face_tracking.py
@@ -8,8 +8,7 @@ import os
 
 from tensorflow.keras.preprocessing import image
 # import torchvision.transforms as transforms
-# from keras.models import load_model
-# emotion_model = load_model('model.h5')
+from tensorflow.keras.models import load_model
 
 print(torch.__version__)
 
@@ -19,74 +18,76 @@ print('Running on device: {}'.format(device))
 def process_video(filename):
   # since MTCNN is a collection of neural nets and other code, the device must be passed in the following way to enable copying of objects when needed internally.
   mtcnn = MTCNN(keep_all=True, device=device)
+
+  emotion_model = load_model('model.h5')
   
-  root_dir = os.getcwd()
-  if (os.path.isdir(f'{os.getcwd()}/movieclassifier_new')):
-    root_dir = f'{os.getcwd()}/movieclassifier_new'
+  # root_dir = os.getcwd()
+  # if (os.path.isdir(f'{os.getcwd()}/movieclassifier_new')):
+  #   root_dir = f'{os.getcwd()}/movieclassifier_new'
 
-  static_files_path = f'{root_dir}/static'
-  filename, file_extension = os.path.splitext(filename)
+  # static_files_path = f'{root_dir}/static'
+  # filename, file_extension = os.path.splitext(filename)
 
-  #loading a video with some faces in it. The mmcv PyPI package by mmlabs is used to read the video frames (it can be installed with pip install mmcv). Frames are then converted to PIL images
-  # movieclassifier_new is the name of the root project directory on the hosting
-  video = mmcv.VideoReader(f'{static_files_path}/untracked/{filename}{file_extension}')
-  frames = [Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)) for frame in video]
+  # #loading a video with some faces in it. The mmcv PyPI package by mmlabs is used to read the video frames (it can be installed with pip install mmcv). Frames are then converted to PIL images
+  # # movieclassifier_new is the name of the root project directory on the hosting
+  # video = mmcv.VideoReader(f'{static_files_path}/untracked/{filename}{file_extension}')
+  # frames = [Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)) for frame in video]
 
-  # from IPython.display import HTML
-  # from base64 import b64encode
-  # mp4 = open('uploads/video.mp4','rb').read()
-  # data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
-  # HTML("""
-  # <video width=400 controls>
-  #       <source src="%s" type="video/mp4">
-  # </video>
-  # """ % data_url)
+  # # from IPython.display import HTML
+  # # from base64 import b64encode
+  # # mp4 = open('uploads/video.mp4','rb').read()
+  # # data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
+  # # HTML("""
+  # # <video width=400 controls>
+  # #       <source src="%s" type="video/mp4">
+  # # </video>
+  # # """ % data_url)
 
-  # iterate through each frame, detect faces, and draw their bounding boxes on the video frames.
-  frames_tracked = []
-  for i, frame in enumerate(frames):
-    # if i < 35 or i > 70:
-    if i < 35 or i > 40:
-      continue
-    print('\rTracking frame: {}'.format(i + 1), end='')
-    # if i < 10:
-      # print(frame)
+  # # iterate through each frame, detect faces, and draw their bounding boxes on the video frames.
+  # frames_tracked = []
+  # for i, frame in enumerate(frames):
+  #   # if i < 35 or i > 70:
+  #   if i < 35 or i > 40:
+  #     continue
+  #   print('\rTracking frame: {}'.format(i + 1), end='')
+  #   # if i < 10:
+  #     # print(frame)
     
-    # Detect faces
-    boxes, _ = mtcnn.detect(frame)
-    # if i < 1:
-    #   frame.save('frame.jpg')
-    #   print(boxes)
+  #   # Detect faces
+  #   boxes, _ = mtcnn.detect(frame)
+  #   # if i < 1:
+  #   #   frame.save('frame.jpg')
+  #   #   print(boxes)
     
-    # Draw faces
-    frame_draw = frame.copy()
-    draw = ImageDraw.Draw(frame_draw)
-    if boxes is not None:
-      for j, box in enumerate(boxes):
-        print(box.tolist())
-        draw.rectangle(box.tolist(), outline=(255, 0, 0), width=6)
-        cropped = frame.crop(box.tolist())
-        predict_emotion(cropped)
-        # cropped.save(f"cropped_{i}-{j}.jpg")
-        print(cropped)
+  #   # Draw faces
+  #   frame_draw = frame.copy()
+  #   draw = ImageDraw.Draw(frame_draw)
+  #   if boxes is not None:
+  #     for j, box in enumerate(boxes):
+  #       print(box.tolist())
+  #       draw.rectangle(box.tolist(), outline=(255, 0, 0), width=6)
+  #       cropped = frame.crop(box.tolist())
+  #       predict_emotion(cropped)
+  #       # cropped.save(f"cropped_{i}-{j}.jpg")
+  #       print(cropped)
     
-    # Add to frame list
-    frames_tracked.append(frame_draw.resize((640, 360), Image.BILINEAR))
-    print('\nDone')
+  #   # Add to frame list
+  #   frames_tracked.append(frame_draw.resize((640, 360), Image.BILINEAR))
+  #   print('\nDone')
 
-  # Save tracked video
-  dim = frames_tracked[0].size
-  fourcc = cv2.VideoWriter_fourcc(*'FMP4')
-  # fourcc = cv2.VideoWriter_fourcc(*'H264')
-  # fourcc = cv2.VideoWriter_fourcc(*'avc1')
-  # fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-  # fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
-  # fourcc = cv2.VideoWriter_fourcc(*'PIM1')
-  video_tracked = cv2.VideoWriter(f'{static_files_path}/tracked/{filename}_tracked.mp4', fourcc, 25.0, dim)
-  for frame in frames_tracked:
-    print(frame)
-    video_tracked.write(cv2.cvtColor(np.array(frame), cv2.COLOR_RGB2BGR))
-  video_tracked.release()
+  # # Save tracked video
+  # dim = frames_tracked[0].size
+  # fourcc = cv2.VideoWriter_fourcc(*'FMP4')
+  # # fourcc = cv2.VideoWriter_fourcc(*'H264')
+  # # fourcc = cv2.VideoWriter_fourcc(*'avc1')
+  # # fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+  # # fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
+  # # fourcc = cv2.VideoWriter_fourcc(*'PIM1')
+  # video_tracked = cv2.VideoWriter(f'{static_files_path}/tracked/{filename}_tracked.mp4', fourcc, 25.0, dim)
+  # for frame in frames_tracked:
+  #   print(frame)
+  #   video_tracked.write(cv2.cvtColor(np.array(frame), cv2.COLOR_RGB2BGR))
+  # video_tracked.release()
 
 def predict_emotion(photo):
   print('sdf')
