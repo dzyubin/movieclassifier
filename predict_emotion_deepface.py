fd93fd1ec85264df4636937b488b02a94a6cec22
Volodymyr Dziubin <vovadzyubin@gmail.com>
2022-04-25 09:55:01 +0300

    update

--- predict_emotion_deepface.py
+++ predict_emotion_deepface.py
@@ -1,5 +1,4 @@
 from deepface import DeepFace
-from tensorflow.keras.preprocessing import image
 import cv2
 
 def predict_emotion_deepface(file):
@@ -9,6 +8,7 @@ def predict_emotion_deepface(file):
 #   img = image.load_img(file, color_mode="grayscale", target_size=(48, 48))
   # img = image.load_img(file)
   img = cv2.imread(file)
+  print(img)
   predictions = DeepFace.analyze(img, enforce_detection=False)
 
   return predictions
